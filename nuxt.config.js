608051ef41cf5636216706a049fbdab0f4298f95
Nathan Lei <woooms@qq.com>
2021-10-17 20:13:22 +0800

    asa

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,5 +7,13 @@ export default defineNuxtConfig({
   // Your existing configuration
   server: {
     host: '0.0.0.0'
+  },
+  modules: ['@nuxtjs/strapi'],
+  strapi: {
+    // Options
+    entities: ['orders'],
+    expires: '31d'
+
   }
+
 })
